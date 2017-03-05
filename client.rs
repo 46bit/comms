8bdf4ff383ee7a8aae810a48d2adbc8fc6a31e2c
Michael Mokrysz <hi@46b.it>
2017-03-05 03:58:31 +0000

    Building out `comms` stack with Room impl and Relay structure.

---
+++ client.rs
@@ -0,0 +1,247 @@
+use futures::{Sink, Future};
+use errors::Error;
+use super::*;
+use net::Msg;
+use futures::sync::oneshot;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Client<S, E>
+    where S: Sink<SinkItem = Command, SinkError = E> + Send + Clone + 'static,
+          E: Into<Error> + 'static
+{
+    // Clients are identified by UUID.
+    id: ClientId,
+    // Clients can also have an ID specific to their communication form, e.g., SocketAddr.
+    name: Option<String>,
+    // Channel to command communications with.
+    cmd_tx: CommandChannel<S>,
+}
+
+impl<S, E> Client<S, E>
+    where S: Sink<SinkItem = Command, SinkError = E> + Send + Clone + 'static,
+          E: Into<Error> + 'static
+{
+    pub fn new(id: ClientId,
+               name: Option<String>,
+               cmd_tx: CommandChannel<S>)
+               -> Result<Client<S, E>, Error> {
+        if !cmd_tx.can_command(&id) {
+            return Err(format!("Attempted to add a client to a room using a different listener")
+                .into());
+        }
+        Ok(Client {
+            id: id,
+            name: name,
+            cmd_tx: cmd_tx,
+        })
+    }
+
+    pub fn id(&self) -> ClientId {
+        self.id
+    }
+
+    pub fn name(&self) -> Option<String> {
+        self.name.clone()
+    }
+
+    fn command(&mut self, cmd: Command) -> Box<Future<Item = (), Error = Error>> {
+        Box::new(self.cmd_tx.clone().send(cmd).map(|_| ()).map_err(|e| e.into()))
+    }
+}
+
+impl<S, E> Communicator for Client<S, E>
+    where S: Sink<SinkItem = Command, SinkError = E> + Send + Clone + 'static,
+          E: Into<Error> + 'static
+{
+    type Transmit = Msg;
+    type Receive = Msg;
+    type Status = ClientStatus;
+    type Error = Error;
+
+    fn transmit(&mut self, msg: Self::Transmit) -> Box<Future<Item = (), Error = Error>> {
+        let cmd = Command::Transmit(self.id(), msg);
+        Box::new(self.command(cmd))
+    }
+
+    fn receive(&mut self,
+               timeout: ClientTimeout)
+               -> Box<Future<Item = Self::Receive, Error = Error>> {
+        let (msg_forward_tx, msg_forward_rx) = oneshot::channel();
+        let cmd = Command::ReceiveInto(self.id(), msg_forward_tx, timeout);
+        Box::new(self.command(cmd).and_then(|_| msg_forward_rx.map_err(|e| e.into())))
+    }
+
+    fn status(&mut self) -> Box<Future<Item = Self::Status, Error = Error>> {
+        let (status_tx, status_rx) = oneshot::channel();
+        let cmd = Command::StatusInto(self.id(), status_tx);
+        Box::new(self.command(cmd).and_then(|_| status_rx.map_err(|e| e.into())))
+    }
+
+    fn close(&mut self) -> Box<Future<Item = (), Error = Error>> {
+        let cmd = Command::Close(self.id());
+        Box::new(self.command(cmd))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use uuid::Uuid;
+    use futures::sync::mpsc;
+    use futures::{Stream, executor};
+    use std::sync::Arc;
+    use net::Msg;
+
+    fn unpark_noop() -> Arc<executor::Unpark> {
+        struct Foo;
+
+        impl executor::Unpark for Foo {
+            fn unpark(&self) {}
+        }
+
+        Arc::new(Foo)
+    }
+
+    fn mock_command_channel(tx: mpsc::Sender<Command>) -> CommandChannel<mpsc::Sender<Command>> {
+        CommandChannel {
+            id: Uuid::new_v4(),
+            cmd_tx: tx,
+        }
+    }
+
+    fn mock_client(command_channel: &CommandChannel<mpsc::Sender<Command>>)
+                   -> Client<mpsc::Sender<Command>, mpsc::SendError<Command>> {
+        let client_id = ClientId {
+            client: Uuid::new_v4(),
+            communicator: command_channel.id(),
+        };
+        Client::new(client_id, None, command_channel.clone()).unwrap()
+    }
+
+    #[test]
+    fn can_transmit() {
+        let (tx, rx) = mpsc::channel(1);
+        let mut rx_stream = rx.wait().peekable();
+        let mut client = mock_client(&mock_command_channel(tx));
+
+        for _ in 0..10 {
+            let msg = Msg::version();
+            client.transmit(msg.clone()).wait().unwrap();
+
+            match rx_stream.next() {
+                Some(Ok(Command::Transmit(client_id, msg2))) => {
+                    assert!(client_id == client.id());
+                    assert!(msg == msg2);
+                }
+                _ => assert!(false),
+            }
+        }
+    }
+
+    #[test]
+    fn can_receive() {
+        let (tx, rx) = mpsc::channel(1);
+        let mut rx_stream = rx.wait().peekable();
+        let mut client = mock_client(&mock_command_channel(tx));
+
+        for _ in 0..10 {
+            let msg = Msg::version();
+            let receive = client.receive(ClientTimeout::None);
+
+            let mut future = executor::spawn(receive.fuse());
+
+            assert!(future.poll_future(unpark_noop()).unwrap().is_not_ready());
+
+            match rx_stream.next().unwrap().unwrap() {
+                Command::ReceiveInto(client_id, msg_forward_tx, ClientTimeout::None) => {
+                    assert!(client_id == client.id());
+                    msg_forward_tx.complete(msg.clone());
+                }
+                _ => assert!(false),
+            }
+
+            match future.wait_future() {
+                Ok(msg2) => assert!(msg == msg2),
+                _ => assert!(false),
+            }
+        }
+    }
+
+    #[test]
+    fn can_receive_queued() {
+        let (tx, rx) = mpsc::channel(1);
+        let mut rx_stream = rx.wait().peekable();
+        let mut client = mock_client(&mock_command_channel(tx));
+
+        let mut futures = vec![];
+
+        for _ in 0..10 {
+            let msg = Msg::version();
+            let mut future = executor::spawn(client.receive(ClientTimeout::None));
+            assert!(future.poll_future(unpark_noop()).unwrap().is_not_ready());
+            futures.push((msg, future));
+        }
+
+        for &mut (ref mut msg, _) in &mut futures {
+            match rx_stream.next().unwrap().unwrap() {
+                Command::ReceiveInto(client_id, msg_forward_tx, ClientTimeout::None) => {
+                    assert!(client_id == client.id());
+                    msg_forward_tx.complete(msg.clone());
+                }
+                _ => assert!(false),
+            }
+        }
+
+        for &mut (ref mut msg, ref mut future) in &mut futures {
+            match future.wait_future() {
+                Ok(msg2) => assert!(msg.clone() == msg2),
+                _ => assert!(false),
+            }
+        }
+    }
+
+    #[test]
+    fn can_status() {
+        let (tx, rx) = mpsc::channel(1);
+        let mut rx_stream = rx.wait().peekable();
+        let mut client = mock_client(&mock_command_channel(tx));
+
+        for _ in 0..10 {
+            let mut future = executor::spawn(client.status());
+
+            assert!(future.poll_future(unpark_noop()).unwrap().is_not_ready());
+
+            match rx_stream.next().unwrap().unwrap() {
+                Command::StatusInto(client_id, status_reply_tx) => {
+                    assert!(client_id == client.id());
+                    status_reply_tx.complete(ClientStatus::Ready);
+                }
+                _ => assert!(false),
+            }
+
+            match future.wait_future() {
+                Ok(status) => assert!(status == ClientStatus::Ready),
+                _ => assert!(false),
+            }
+        }
+    }
+
+    #[test]
+    fn can_close() {
+        let (tx, rx) = mpsc::channel(1);
+        let mut rx_stream = rx.wait().peekable();
+        let mut client = mock_client(&mock_command_channel(tx));
+
+        for _ in 0..10 {
+            let msg = Msg::version();
+            client.transmit(msg.clone()).wait().unwrap();
+            match rx_stream.next() {
+                Some(Ok(Command::Transmit(client_id, msg2))) => {
+                    assert!(client_id == client.id());
+                    assert!(msg == msg2);
+                }
+                _ => assert!(false),
+            }
+        }
+    }
+}
---
+++ mod.rs
@@ -0,0 +1,121 @@
+mod client;
+mod room;
+mod relay;
+
+pub use self::client::*;
+pub use self::room::*;
+pub use self::relay::*;
+
+use futures::{Future, Sink, StartSend, Poll};
+use uuid::Uuid;
+use net::Msg;
+use std::collections::{HashSet, HashMap};
+use std::time::Duration;
+use futures::sync::oneshot;
+
+#[derive(Hash, Copy, Clone, Debug, PartialEq, Eq)]
+pub struct ClientId {
+    client: Uuid,
+    communicator: Uuid,
+}
+
+impl ClientId {
+    pub fn client_id(&self) -> Uuid {
+        self.client
+    }
+
+    pub fn communicator_id(&self) -> Uuid {
+        self.communicator
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ClientTimeout {
+    None,
+    Optional(Duration),
+    Disconnecting(Duration),
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum ClientStatus {
+    Ready,
+    Waiting,
+    Gone,
+}
+
+pub enum Command {
+    // Send a message to a single client.
+    Transmit(ClientId, Msg),
+    // Send specific messages to specific clients.
+    TransmitToGroup(HashMap<ClientId, Msg>),
+    // Send a message to all clients on the other end.
+    Broadcast(Msg),
+    // Receive a message from a single client into a `oneshot::Receiver`.
+    ReceiveInto(ClientId, oneshot::Sender<Msg>, ClientTimeout),
+    // Receive one message from each specified clients into `oneshot::Receiver`s.
+    ReceiveFromGroupInto(HashSet<ClientId>, oneshot::Sender<HashMap<ClientId, Msg>>, ClientTimeout),
+    // Discard all messages already received from a client.
+    DiscardReceiveBuffer(ClientId),
+    // Discard all messages already received from specified clients.
+    DiscardReceiveBufferForGroup(HashSet<ClientId>),
+    // Receive a message from a single client into a `oneshot::Receiver`.
+    StatusInto(ClientId, oneshot::Sender<ClientStatus>),
+    // Receive one message from each specified clients into `oneshot::Receiver`s.
+    StatusFromGroupInto(HashSet<ClientId>, oneshot::Sender<HashMap<ClientId, ClientStatus>>),
+    // Disconnect a single client.
+    Close(ClientId),
+    // Disconnect specified clients.
+    CloseGroup(HashSet<ClientId>),
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct CommandChannel<C>
+    where C: Sink<SinkItem = Command> + Send + Clone + 'static
+{
+    id: Uuid,
+    cmd_tx: C,
+}
+
+impl<C> CommandChannel<C>
+    where C: Sink<SinkItem = Command> + Send + Clone + 'static
+{
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+
+    pub fn can_command(&self, client_id: &ClientId) -> bool {
+        self.id == client_id.communicator_id()
+    }
+}
+
+impl<C> Sink for CommandChannel<C>
+    where C: Sink<SinkItem = Command> + Send + Clone + 'static
+{
+    type SinkItem = C::SinkItem;
+    type SinkError = C::SinkError;
+
+    fn start_send(&mut self, item: Self::SinkItem) -> StartSend<Self::SinkItem, Self::SinkError> {
+        self.cmd_tx.start_send(item)
+    }
+
+    fn poll_complete(&mut self) -> Poll<(), Self::SinkError> {
+        self.cmd_tx.poll_complete()
+    }
+}
+
+pub trait Communicator {
+    type Transmit;
+    type Receive;
+    type Status;
+    type Error;
+
+    fn transmit(&mut self, msg: Self::Transmit) -> Box<Future<Item = (), Error = Self::Error>>;
+
+    fn receive(&mut self,
+               optionality: ClientTimeout)
+               -> Box<Future<Item = Self::Receive, Error = Self::Error>>;
+
+    fn status(&mut self) -> Box<Future<Item = Self::Status, Error = Self::Error>>;
+
+    fn close(&mut self) -> Box<Future<Item = (), Error = Self::Error>>;
+}
